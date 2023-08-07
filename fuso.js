47e18dfb92106a044b70761c0ac0abe593d4f7f1
Luiz Francisco <luizfk.gmail.com>
2023-08-07 05:51:44 -0300

    Adicionando novos exercicios

---
+++ fuso.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault()
+
+        const horaBrasil = Number($('#inHoraBrasil').val())
+        let horaFranca = horaBrasil + 5
+
+        if(horaFranca > 24) {
+            horaFranca = horaFranca - 24
+        }
+
+        $('h3').text(`A hora na França é ${horaFranca.toFixed(2)}`);
+
+    })
+})
